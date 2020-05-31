d0088b11d5bf314fee3685b2b31d0e8d365e8d73
Philip A Senger <philip.a.senger@cngrgroup.com>
2020-05-31 19:31:10 +1000

    Simple Milestone
    * Stacks random sized blocks
    * allows moving and crashing.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 import React, {Component} from 'react';
-import {Events,Engine,Render,World,Bodies,Mouse,MouseConstraint} from "matter-js";
+import {
+  Bodies,
+  Common,
+  Composites,
+  Engine,
+  Mouse,
+  MouseConstraint,
+  Render,
+  Runner,
+  World
+} from "matter-js";
 
 class App extends Component {
   constructor(props) {
@@ -10,57 +20,62 @@ class App extends Component {
 
   componentDidMount() {
 
-    const engine = Engine.create({
-      // positionIterations: 20
-    });
-
+    const engine = Engine.create();
     const render = Render.create({
       element: this.sceneRef.current,
       engine: engine,
       options: {
         width: 800,
-        height: 600,
-        wireframes: true,
-        showVelocity: true,
-        showAngleIndicator: true
+        height: 420,
+        wireframes: false,
+        // showVelocity: true,
+        // showAngleIndicator: true
       }
     });
+    Render.run(render);
+
+    const runner = Runner.create();
+    Runner.run(runner, engine);
 
     // walls.
     World.add(engine.world, [
-      Bodies.rectangle(400, 0, 800, 50, { isStatic: true }),
-      Bodies.rectangle(400, 600, 800, 50.5, { isStatic: true }),
-      Bodies.rectangle(800, 300, 50, 600, { isStatic: true }),
-      Bodies.rectangle(0, 300, 50, 600, { isStatic: true })
+      Bodies.rectangle(420, 0, 1200, 50, {isStatic: true}),
+      Bodies.rectangle(400, 600, 1200, 50.5, {isStatic: true}),
+      Bodies.rectangle(960, 300, 50, 600, {isStatic: true}),
+      Bodies.rectangle(-160, 300, 50, 600, {isStatic: true})
     ]);
 
-    // balls.
-    World.add(engine.world, [
-      Bodies.circle(210, 100, 30, { restitution: 0.5 }),
-      Bodies.circle(110, 50, 30, { restitution: 0.5 })
-    ]);
+    engine.world.gravity.y = 1; // -1 makes it anti gravity
 
-    // add mouse control
-    const mouse = Mouse.create(render.canvas),
-        mouseConstraint = MouseConstraint.create(engine, {
-          mouse: mouse,
-          constraint: {
-            stiffness: 0.2,
-            render: {
-              visible: false
-            }
-          }
-        });
+    const stack = Composites.stack(200, 350, 1, 7, 0, 0, function (x, y) {
+      if (Common.random() < 0.8) {
+        return Bodies.rectangle(x, y, Common.random(80, 120), Common.random(20, 30));
+      } else {
+        return Bodies.rectangle(x + 6, y, Common.random(80, 120), Common.random(20, 30));
+      }
+    });
+    World.add(engine.world, stack);
 
+    const mouse = Mouse.create(render.canvas);
+    const mouseConstraint = MouseConstraint.create(engine, {
+      mouse: mouse,
+      constraint: {
+        stiffness: 0.2,
+        render: {
+          visible: false
+        }
+      }
+    });
     World.add(engine.world, mouseConstraint);
 
-    Events.on(mouseConstraint, "mousedown", function(event) {
-      World.add(engine.world, Bodies.circle(150, 50, 30, { restitution: 0.7 }));
+    render.mouse = mouse;
+
+    // fit the render viewport to the scene
+    Render.lookAt(render, {
+      min: {x: 0, y: 0},
+      max: {x: 800, y: 600}
     });
 
-    Engine.run(engine);
-
-    Render.run(render);
   }
 
   render() {
